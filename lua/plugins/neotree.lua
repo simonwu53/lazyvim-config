ea0bb130eb7dbcb571067665aaa4e4cd476dfff7
Shan Wu <Shan.Wu@ut.ee>
2025-01-15 18:34:13 +0200

    fix neotree config to support code symbols display

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,4 +1,18 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+  opts = {
+    sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+    document_symbols = {
+      follow_cursor = true,
+    },
+  },
+  keys = {
+    {
+      "<leader>ce",
+      function()
+        require("neo-tree.command").execute({ source = "document_symbols", toggle = true })
+      end,
+      desc = "Symbol Explorer",
+    },
+  }
 }
